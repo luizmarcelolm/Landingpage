a3b71b7a2dae28d07e95f6f48becc1389111c845
luiz <luizmarcelolm@hotmail.com>
2024-03-01 21:43:41 -0300

    LandingPageInformacoes

---
+++ index.js
@@ -0,0 +1,19 @@
+var setaEsquerda = window.document.getElementById("setaEsquerda")
+var setaDireita = window.document.getElementById("setaDireita")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+
+function rolarParaDireita() {
+    leonardo.style = "display: none"
+    bruna.style = "display: flex"
+    setaDireita.style = "display: none"
+    setaEsquerda.style = "display: flex; margin-top:55px"
+}
+
+function rolarParaEsquerda() {
+    leonardo.style = "display: flex"
+    bruna.style = "display: none"
+    setaDireita.style = "display: flex"
+    setaEsquerda.style = "display: none"
+}
